83f405b62ec01237fb3b571c551beac99d06d648
Kavyasri Peddoju <kavyasripeddoju@gmail.com>
2024-01-24 20:03:38 +0530

    my first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import Products from './component/Products';
+import NotFound from './component/NotFound';
+import LoginForm from './component/LoginForm';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/products" component={Products} />
+    <Route component={NotFound} />
+  </Switch>
+  </BrowserRouter>
+    </div>
+  );
+}
+
+export default App
